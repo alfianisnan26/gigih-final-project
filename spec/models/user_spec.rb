b9afb9ed46c39928947e52cb031c803dda933e38
Alfian Isnan <49936990+alfianisnan26@users.noreply.github.com>
2022-04-23 03:30:23 +0000

    [S2.7] Generating pending rspec for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
+#       t.string :name
+#       t.string :email
+#       t.string :password
+#       t.string :phone
+#       t.string :address
+#       t.boolean :is_admin
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'has many invoices' do
+    should respond_to(:invoices)
+  end
+
+  pending 'has valid data'
+  pending 'is invalid without a name'
+  pending 'is invalid with name\'s letter length more than 64'
+  pending 'is invalid with name\'s letter length less than 2'
+  pending 'is invalid without an email'
+  pending 'is invalid when email is malformatted'
+  pending 'is invalid without a password'
+  pending 'is invalid without a phone'
+  pending 'is valid without address and set to blank'
+  pending 'is valid without is_admin and set to false'
+  
 end
