36cd83e4e3c34c58396f2600bea92db1d9f92cab
Alfian Isnan <49936990+alfianisnan26@users.noreply.github.com>
2022-04-23 03:35:01 +0000

    [S2.7.2] Refactoring and adding some code to rspec for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,15 +12,26 @@ RSpec.describe User, type: :model do
   end
 
   pending 'has valid data'
-  pending 'is invalid without a name'
-  pending 'is invalid with name\'s letter length more than 64'
-  pending 'is invalid with name\'s letter length less than 2'
-  pending 'is invalid without an email'
-  pending 'is invalid when email is malformatted'
-  pending 'is invalid without a password'
-  pending 'is invalid without a phone'
-  pending 'is invalid if generated password equals to plain-password'
-  pending 'is valid if password hash are equals when checking password'
+  describe 'name' do
+    pending 'is invalid without a name'
+    pending 'is invalid with name\'s letter length more than 64'
+    pending 'is invalid with name\'s letter length less than 2'
+  end
+  describe 'email' do
+    pending 'is invalid without an email'
+    pending 'is invalid when email is malformatted'
+    pending 'is invalid with duplicate email'
+  end
+  describe 'password' do
+    pending 'is invalid without a password'
+    pending 'is invalid if generated password equals to plain-password'
+    pending 'is valid if password hash are equals when checking password'
+  end
+  describe 'phone' do
+    pending 'is invalid without a phone'
+    pending 'is invalid when phone is malformatted'
+    pending 'is invalid with duplicate phone'
+  end
   pending 'is valid without address and set to blank'
   pending 'is valid without is_admin and set to false'
   
