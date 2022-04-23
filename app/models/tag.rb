1e91fd54101ef216b4e0b1ae57e99df589645df1
Alfian Isnan <49936990+alfianisnan26@users.noreply.github.com>
2022-04-23 03:03:30 +0000

    [S2.5.1] Resolving rspec for tag

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,5 @@
 class Tag < ApplicationRecord
-    has_many :tag_relations
-    has_many :menus, through: :tag_relations
+  validates :name, presence: true, uniqueness: true, length: {minimum: 2, maximum: 150}
+  has_many :tag_relations
+  has_many :menus, through: :tag_relations
 end
